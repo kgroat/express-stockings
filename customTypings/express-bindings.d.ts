82f05ca81e853d5616f977a78a0beade95688d0c
Kevin Groat <apps@kevingroat.io>
2017-09-09 18:47:55 -0400

    Improved subscribe typescript bindings

--- customTypings/express-bindings.d.ts
+++ customTypings/express-bindings.d.ts
@@ -1,13 +1,20 @@
 import 'express'
 import { MergeStrategy } from 'stockings'
 
+type SubscribeMethod = 
+  ((eventId: string) => number) |
+  ((eventId: string, mergeStrategy: 'replace') => number) |
+  ((eventId: string, mergeStrategy: 'append') => number) |
+  ((eventId: string, mergeStrategy: 'prepend') => number) |
+  ((eventId: string, mergeStrategy: 'upsert', upsertKey: string) => number)
+
 declare global {
   namespace Express {
     interface Request {
       hasStockingsClient: () => boolean
     }
     interface Response {
-      subscribe: (eventId: string, mergeStrategy?: MergeStrategy, upsertKey?: string) => number
+      subscribe: SubscribeMethod
       broadcast: <T>(eventId: string, payload: T, cb?: (err: any) => void) => void
     }
   }
