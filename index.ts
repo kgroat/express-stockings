5ada16158fc8001873ca794608ea9fd50026a3ce
Kevin Groat <apps@kevingroat.io>
2017-09-09 12:44:33 -0400

    Fixed lint errors

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import * as http from 'http'
 import { Request, Response, RequestHandler, NextFunction } from 'express'
-import { Server as StockingsServer, ServerOptions, ConnectionRequest } from 'stockings'
-import { StockingsConnection} from 'stockings/src/stockingsConnection'
+import { Server as StockingsServer, ConnectionRequest } from 'stockings'
+import { StockingsConnection } from 'stockings/src/stockingsConnection'
 import './customTypings/express-bindings'
 
 const TOKEN_HEADER = 'client-token'
 const SUBSCRIPTION_HEADER = 'client-subscriptions'
 
-export var Connection = StockingsConnection
+export const Connection = StockingsConnection
 
 export interface MiddlewareOptions {
   server: http.Server | number
@@ -18,20 +18,20 @@ export interface MiddlewareOptions {
   disposeMetaAfter?: number
 }
 
-export function middleware(options: MiddlewareOptions): RequestHandler {
-  function getAddress(req: Request){
+export function middleware (options: MiddlewareOptions): RequestHandler {
+  function getAddress (req: Request) {
     return req.header('x-forwarded-for') || req.connection.remoteAddress
   }
 
-  var httpServer: http.Server
-  if(typeof options.server === 'number'){
+  let httpServer: http.Server
+  if (typeof options.server === 'number') {
     httpServer = http.createServer()
     httpServer.listen(options.server)
   } else {
     httpServer = options.server
   }
 
-  var stockings = new StockingsServer({
+  const stockings = new StockingsServer({
     server: httpServer,
     privateKey: options.privateKey,
     publicKey: options.publicKey,
@@ -40,23 +40,23 @@ export function middleware(options: MiddlewareOptions): RequestHandler {
     disposeMetaAfter: options.disposeMetaAfter
   })
 
-  return function middleware(req: Request, res: Response, next: NextFunction) {
-    var token = req.header(TOKEN_HEADER)
-    var client: StockingsConnection
-    var transactionId: string
+  return function middleware (req: Request, res: Response, next: NextFunction) {
+    const token = req.header(TOKEN_HEADER)
+    let client: StockingsConnection
+    let transactionId: string
 
     req.hasClient = () => {
       return !!client
     }
 
-    res.subscribe = (type: string, mergeStrategy?: (a,b)=>any): number => {
-      if(!client){
+    res.subscribe = (type: string, mergeStrategy?: (a,b) => any): number => {
+      if (!client) {
         return 0
       }
-      if(!transactionId){
+      if (!transactionId) {
         transactionId = client.generateTransactionId()
       }
-      var count = client.addSubscription(type, transactionId, mergeStrategy)
+      const count = client.addSubscription(type, transactionId, mergeStrategy)
       res.setHeader(SUBSCRIPTION_HEADER, client.getSubscriptionHeader(transactionId))
       return count
     }
@@ -64,7 +64,7 @@ export function middleware(options: MiddlewareOptions): RequestHandler {
       stockings.sendData(type, payload, cb)
     }
 
-    if(token){
+    if (token) {
       stockings.getConnection(token, getAddress(req)).then((_client) => {
         client = _client
         next()
